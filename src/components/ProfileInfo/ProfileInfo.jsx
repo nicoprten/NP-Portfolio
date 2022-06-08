f04ad1fd10a4db603b7688e85ff825794310e1e6
Nicolas Prtenjaca <nicoprtenjaca@gmail.com>
2022-06-08 13:04:31 -0300

    AHORA CREO Q YA ESTA

--- src/components/ProfileInfo/ProfileInfo.jsx
+++ src/components/ProfileInfo/ProfileInfo.jsx
@@ -6,8 +6,9 @@ export default function ProfileInfo({profileActive}){
             <div className="profile-info animate__animated animate__backInLeft">
                 <h1>Nicolás Prtenjaca</h1>
                 <h2>Frontend Developer</h2>
+                <p><span>STACK:</span> HTML, CSS, SASS, Bootstrap, Tailwind, Javascript, JQuery, React, Redux, APIs.</p>
             </div>
-            <div className='inactive'>
+            <div className='container-button'>
                 <button onClick={() => window.open('./CV-v5-english.pdf')}>DOWNLOAD RESUME</button>
             </div>
         </div>
