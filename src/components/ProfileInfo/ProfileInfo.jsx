750a95ee839ec31f019e2a4f554e4604e890d5dd
Nicolas Prtenjaca <nicoprtenjaca@gmail.com>
2022-06-08 14:18:14 -0300

    Adding skill material ui

--- src/components/ProfileInfo/ProfileInfo.jsx
+++ src/components/ProfileInfo/ProfileInfo.jsx
@@ -6,7 +6,7 @@ export default function ProfileInfo({profileActive}){
             <div className="profile-info">
                 <h1>Nicolás Prtenjaca</h1>
                 <h2>Frontend Developer</h2>
-                <p><span>STACK:</span> HTML, CSS, SASS, Bootstrap, Tailwind, Javascript, JQuery, React, Redux, APIs, NPM, GitHub, Photoshop, Illustrator, AfterEffects.</p>
+                <p><span>STACK:</span> HTML, CSS, SASS, Bootstrap, Tailwind, Material UI, Javascript, JQuery, React, Redux, APIs, NPM, GitHub, Photoshop, Illustrator, AfterEffects.</p>
             </div>
             <div className='container-button'>
                 <button onClick={() => window.open('./CV-v5-english.pdf')}>DOWNLOAD RESUME</button>
