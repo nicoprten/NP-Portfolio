bcc5acff9096f6616db3f1a3c046e8310d4d85f2
Nicolas Prtenjaca <nicoprtenjaca@gmail.com>
2022-06-30 19:01:41 -0300

    Adding behance icon

--- src/components/SocialLinks/SocialLinks.jsx
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -3,16 +3,18 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import EmailIcon from '@mui/icons-material/Email';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import { BehanceLogo } from 'phosphor-react';
 import style from './SocialLinks.scss';
 
 export default function SocialLinks(){
     return(
         <ul className='social-container'>
-            <li><a href='https://www.linkedin.com/in/nicoprten/' target='_blank'>{<LinkedInIcon sx={{fontSize: 50}}/>}</a></li>
-            <li><a href='https://github.com/nicoprten' target='_blank'>{<GitHubIcon sx={{fontSize: 50}}/>}</a></li>
-            <li><a href='mailto:nicoprtenjaca@gmail.com' target='_blank'>{<EmailIcon sx={{fontSize: 50}}/>}</a></li>
-            <li><a href='https://twitter.com/nikprten' target='_blank'>{<TwitterIcon sx={{fontSize: 50}}/>}</a></li>
-            <li><a href='https://www.instagram.com/designby_np/' target='_blank'>{<InstagramIcon sx={{fontSize: 50}}/>}</a></li>
+            <li><a href='https://www.linkedin.com/in/nicoprten/' target='_blank'>{<LinkedInIcon sx={{fontSize: 45}}/>}</a></li>
+            <li><a href='https://github.com/nicoprten' target='_blank'>{<GitHubIcon sx={{fontSize: 45}}/>}</a></li>
+            <li><a href='mailto:nicoprtenjaca@gmail.com' target='_blank'>{<EmailIcon sx={{fontSize: 45}}/>}</a></li>
+            <li><a href='https://twitter.com/nikprten' target='_blank'>{<TwitterIcon sx={{fontSize: 45}}/>}</a></li>
+            <li><a href='https://www.instagram.com/designby_np/' target='_blank'>{<InstagramIcon sx={{fontSize: 45}}/>}</a></li>
+            <li><a href='https://www.behance.net/nicolasprtenja' target='_blank'>{<BehanceLogo size={45} />}</a></li>
         </ul>
     )
 }
